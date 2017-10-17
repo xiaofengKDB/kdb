5df2ccd221139d1202a08b69d2f9bcbb0bcbd3ae
liufan <327979012@qq.com>
2017-10-17 19:52:34 +0800

    [Bug Id]:0 [Modify]:订单列表开发 [Config]:N

--- src/main/java/com/soecode/lyf/web/OrderController.java
+++ src/main/java/com/soecode/lyf/web/OrderController.java
@@ -1,19 +1,17 @@
 package com.soecode.lyf.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
 
 	@RequestMapping(value="/view",method=RequestMethod.GET)
-	@ResponseBody
-	public ModelAndView view(){
-		ModelAndView mv = new ModelAndView("/orderList");
-		return mv;
+	public String view(Model model){
+//		ModelAndView mv = new ModelAndView("/orderList");
+		return "/orderList.html";
 	}
 }
